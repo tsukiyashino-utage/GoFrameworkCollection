2d510bac57411d440a82df3453171ec3c190cf1a
ninghuilin <ninghuilin@bytedance.com>
2023-12-14 00:44:40 +0800

    web框架 HTTP请求 上下文 前缀树路由 开发完成

---
+++ gee-web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
+	err := r.Run(":9999")
+	if err != nil {
+		return
+	}
+}
